2377c9737fef8a26bdb9cb309085f2eda19cc69b
0neMiss <jordanshehane@gmail.com>
2023-12-10 17:01:10 -0700

    moving some things around

--- src/constants/FormattedTemplates.ts
+++ src/constants/FormattedTemplates.ts
@@ -12,3 +12,21 @@ export const textArtName = String.raw`
   \____/ \___/|_|  \__,_|\__,_|_| |_| |_____/|_| |_|\___|_| |_|\__,_|_| |_|\___|   |            (  /
                                                                                    \`-.._____..-'
 `;
+export const projectsOuputText = String.raw`
+    - A mobile app using React Native for music practice     
+    - Conways game of life using PiGame                         https://github.com/0neMiss/GameOfLife
+  `;
+
+export const linksOutputText = String.raw`
+      - Github        https://github.com/0neMiss?tab=overview
+      - linkedin      https://www.linkedin.com/in/jordan-shehane-b2807a196/
+    `;
+export const contactOutputText = String.raw`
+    If you want to contact me the best way is to send me a message on linkedin!
+
+    - email     jordanshehane@gmail.com
+    - phone     618-946-4327
+    - linkedin  https://www.linkedin.com/in/jordan-shehane-b2807a196/
+    `;
+export const helpOutpueText =
+  "other commands:\n - projects       Provides a list of links to other things I've worked on.\n - contact        How to contact me!\n - links          Links to things like my linkedin and github";
--- src/constants/RecognizedCommands.ts
+++ src/constants/RecognizedCommands.ts
@@ -1,28 +1,21 @@
 import { CommandMap } from "../types/CommandObjects";
+import {
+  contactOutputText,
+  helpOutpueText,
+  linksOutputText,
+  projectsOuputText,
+} from "./FormattedTemplates";
 export const recognizedCommands: CommandMap = {
   help: {
-    outputText:
-      "other commands:\n - projects       Provides a list of links to other things I've worked on.\n - contact        How to contact me!\n - links          Links to things like my linkedin and github",
+    outputText: helpOutpueText,
   },
   projects: {
-    outputText: String.raw`
-    - A mobile app using React Native for music practice     
-    - Conways game of life using PiGame                         https://github.com/0neMiss/GameOfLife
-  `,
+    outputText: projectsOuputText,
   },
   contact: {
-    outputText: String.raw`
-    If you want to contact me the best way is to send me a message on linkedin!
-
-    - email     jordanshehane@gmail.com
-    - phone     618-946-4327
-    - linkedin  https://www.linkedin.com/in/jordan-shehane-b2807a196/
-    `,
+    outputText: contactOutputText,
   },
   links: {
-    outputText: String.raw`
-      - Github        https://github.com/0neMiss?tab=overview
-      - linkedin      https://www.linkedin.com/in/jordan-shehane-b2807a196/
-    `,
+    outputText: linksOutputText,
   },
 };
